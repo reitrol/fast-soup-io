2122976ea6b5af58d1ccc68eb5e30797e4552a76
Pascal Schmid <psschmid@gmx.de>
2013-05-19 13:50:27 +0200

    Renamed website specific content

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Fast soup-io' });
 };
 
 exports.image = function(req, res){
