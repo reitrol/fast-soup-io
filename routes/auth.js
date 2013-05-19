c63fe2fceb5cfe39e56753ac4ff82b8b5c4d30de
Roland <reitrol@gmx.net>
2013-05-19 14:29:10 +0200

    added POST for auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,30 @@
 exports.register = function(req, res){
+
+
+    console.log("get...");
+
     res.render('register', { title: 'Express' });
+
+};
+
+exports.registerSubmit = function(req, res){
+
+
+    console.log("post...");
+
+    var mail = req.param('mail');
+    var pwd = req.param('pwd');
+
+    console.log(mail);
+    console.log(pwd);
+
+
+    //user = GLOBAL.db.newUser(mail, pwd);
+
+    //console.log(user);
+
+
+    res.render('register', { title: 'Express' });
+
 };
 
