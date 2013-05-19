989a072ef1b028ccb262e4d46548b5108600f250
Pascal Schmid <psschmid@gmx.de>
2013-05-19 14:34:55 +0200

    Added about section

--- routes/auth.js
+++ routes/auth.js
@@ -2,3 +2,7 @@ exports.register = function(req, res){
     res.render('register', { title: 'Fast soup-io' });
 };
 
+exports.about = function(req, res){
+    res.render('about', { title: 'Fast soup-io' });
+};
+
